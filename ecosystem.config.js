1d470a181f9850ba3015104e41113ab996c4f77b
jaewoo-21c <jaewoo.c@21market.kr>
2022-11-22 13:39:56 +0900

    [fix]

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       args: 'start',
       exec_mode: 'cluster',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      instances: 0,
+      instances: 1,
       instance_var: 'INSTANCE_ID',
       env: {
         NODE_ENV: 'development',

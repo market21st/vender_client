790e53d41c7707ba93cb9bfa6a0308a3eae9a8b4
jaewoo-21c <jaewoo.c@21market.kr>
2022-11-22 14:10:18 +0900

    [fix] serve express

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,10 @@ module.exports = {
     {
       name: 'vender-client',
       cwd: './',
-      script: 'npm',
-      args: 'start',
+      script: 'server.js',
       exec_mode: 'cluster',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      instances: 1,
+      instances: 0,
       instance_var: 'INSTANCE_ID',
       env: {
         NODE_ENV: 'development',
